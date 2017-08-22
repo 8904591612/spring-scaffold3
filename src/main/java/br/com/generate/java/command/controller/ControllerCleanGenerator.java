53ca604d17d9d1445b20abe59cd9652f435d92c9
Niranjan <niranjandattatreya@am.amrita.edu>
2017-08-22 18:25:01 +0530

    changes

---
+++ src/main/java/br/com/generate/java/command/controller/ControllerCleanGenerator.java
@@ -0,0 +1,28 @@
+package br.com.generate.java.command.controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import br.com.generate.ReadScaffoldInfo;
+
+public class ControllerCleanGenerator extends ReadScaffoldInfo {
+	
+	public ControllerCleanGenerator(String className) throws IOException {
+		generate(className);
+	}	
+	
+	public void generate(String className) throws IOException {
+		File javaTemplateFile = new File(getUserDir() + "/src/main/resources/templates/java/controller/template-clean-controller.txt");
+		String javaStrings = FileUtils.readFileToString(javaTemplateFile);
+		
+		javaStrings = javaStrings.replace("${package}", getPackage() + ".controller");
+		javaStrings = javaStrings.replace("${className}", className);
+		
+		File newJavaFile = new File(getPathPackage() + "controller/" + className + "Controller.java");
+		FileUtils.writeStringToFile(newJavaFile, javaStrings);
+		System.out.println("create " + getPathPackage() + className + "Controller.java");
+	}
+	
+}
